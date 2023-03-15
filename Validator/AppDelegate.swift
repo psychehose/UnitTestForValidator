409061da75560ebf71232d25de23a1df1b8b3ffc
psychehose <psychehose@gmail.com>
2023-03-15 15:59:30 +0900

    Initial Setting

---
+++ Validator/AppDelegate.swift
@@ -0,0 +1,42 @@
+//
+//  AppDelegate.swift
+//  Validator
+//
+//  Created by 김호세 on 2023/03/15.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    return true
+  }
+
+  // MARK: UISceneSession Lifecycle
+
+  func application(
+    _ application: UIApplication,
+    configurationForConnecting connectingSceneSession: UISceneSession,
+    options: UIScene.ConnectionOptions
+  ) -> UISceneConfiguration {
+
+    return UISceneConfiguration(
+      name: "Default Configuration",
+      sessionRole: connectingSceneSession.role
+    )
+  }
+
+  func application(
+    _ application: UIApplication,
+    didDiscardSceneSessions sceneSessions: Set<UISceneSession>
+  ) {
+  }
+
+
+}
+
---
+++ Validator/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Validator
+//
+//  Created by 김호세 on 2023/03/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    print("Hello World")
+  }
+
+}
+
