f37ead95b7c4dfc10e952b7e6b36b2549bfbd3f3
psychehose <psychehose@gmail.com>
2023-03-24 15:49:01 +0900

    ADD: Validator Unit Test

--- Validator/Util/Validator.swift
+++ Validator/Util/Validator.swift
@@ -10,9 +10,6 @@ import Foundation
 public struct Validator {
   public init() { }
   private let idValidationPattern: String = "^[a-z]{1}[a-z0-9]{4,11}$"
-  private let passwordValidationPattern: String = "^(?!([A-Za-z]+|[~!@#$%^&*()_+=]+|(?=[0-9]+))$)[A-Za-z\\d~!@#$%^&*()_+=]{8,12}$"
-  private let contactNumber: String = "^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$"
-  private let emailValidationPattern: String = "^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
 }
 
 public extension Validator {
@@ -22,60 +19,6 @@ public extension Validator {
         pattern: idValidationPattern,
         options: []
       ) else {
-      assertionFailure("Regex not valid")
-      return false
-    }
-    let regexFirstMatch = regex
-      .firstMatch(
-        in: input,
-        options: [],
-        range: NSRange(location: 0, length: input.count)
-      )
-    return regexFirstMatch != nil
-  }
-  func passwordValidateByClient(_ input: String) -> Bool {
-    guard
-      let regex = try? NSRegularExpression(
-        pattern: passwordValidationPattern,
-        options: []
-      )
-    else {
-      assertionFailure("Regex not valid")
-      return false
-    }
-    let regexFirstMatch = regex
-      .firstMatch(
-        in: input,
-        options: [],
-        range: NSRange(location: 0, length: input.count)
-      )
-    return regexFirstMatch != nil
-  }
-  func emailValidateByClient(_ input: String) -> Bool {
-    guard
-      let regex = try? NSRegularExpression(
-        pattern: emailValidationPattern,
-        options: [.caseInsensitive]
-      )
-    else {
-      assertionFailure("Regex not valid")
-      return false
-    }
-    let regexFirstMatch = regex
-      .firstMatch(
-        in: input,
-        options: [],
-        range: NSRange(location: 0, length: input.count)
-      )
-    return regexFirstMatch != nil
-  }
-  func contactNumberValidateByClient(_ input: String) -> Bool {
-    guard
-      let regex = try? NSRegularExpression(
-        pattern: contactNumber,
-        options: [.caseInsensitive]
-      )
-    else {
       assertionFailure("Regex not valid")
       return false
     }
---
+++ ValidatorTests/ValidatorTests.swift
@@ -0,0 +1,111 @@
+//
+//  ValidatorTests.swift
+//  ValidatorTests
+//
+//  Created by 김호세 on 2023/03/15.
+//
+
+import XCTest
+@testable import Validator
+
+final class ValidatorTests: XCTestCase {
+
+  var sut: Validator!
+
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+    sut = Validator()
+  }
+
+  override func tearDownWithError() throws {
+    sut = nil
+    try super.tearDownWithError()
+  }
+
+  func testFailFirstNumberID() throws {
+    let id = "1psychese"
+
+    let result = sut.idValidateByClient(id)
+
+    XCTAssertEqual(result, true)
+  }
+
+  func testFailFirstSpecialSymbolID() throws {
+    let id = "_psychehose"
+
+    let result = sut.idValidateByClient(id)
+    XCTAssertEqual(result, true)
+  }
+
+  func testFailFirstCaptialID() throws {
+    let id = "Psychehose"
+    let result = sut.idValidateByClient(id)
+
+    XCTAssertEqual(result, true)
+  }
+
+  func testFailFirstKoreanID() throws {
+    let id = "김psychehose"
+    let result = sut.idValidateByClient(id)
+
+    XCTAssertEqual(result, true)
+  }
+
+  func testFailOneLetterID() throws {
+    let id = "p"
+    let result = sut.idValidateByClient(id)
+
+    XCTAssertEqual(result, true)
+  }
+
+  func testFailFourLetterLetterID() {
+    let id = "abcd"
+    let result = sut.idValidateByClient(id)
+
+    XCTAssertEqual(result, true)
+  }
+  func testFailThirteenLetterID() throws {
+    let id = "abcde12345678"
+    let result = sut.idValidateByClient(id)
+    XCTAssertEqual(result, true)
+  }
+
+  func testFailIDWithSymbol() throws {
+    let id = "ab!cdef"
+    let result = sut.idValidateByClient(id)
+    XCTAssertEqual(result, true)
+  }
+
+  func testSuccessFiveLetterID() throws {
+    let id = "abcde"
+    let result = sut.idValidateByClient(id)
+
+    XCTAssertEqual(result, true)
+  }
+  func testSuccessID() throws {
+    let id = "psychehose"
+    let result = sut.idValidateByClient(id)
+
+    XCTAssertEqual(result, true)
+  }
+
+  func testSuccessTwelveLetterID() throws {
+    let id = "abcdeabcdeab"
+    let result = sut.idValidateByClient(id)
+
+    XCTAssertEqual(result, true)
+  }
+
+  func testSuccessIDWithNumber1() throws {
+    let id = "abcd1234"
+    let result = sut.idValidateByClient(id)
+
+    XCTAssertEqual(result, true)
+  }
+  func testSuccessIDWithNumber2() throws {
+    let id = "a1b2c3d4"
+    let result = sut.idValidateByClient(id)
+
+    XCTAssertEqual(result, true)
+  }
+}
